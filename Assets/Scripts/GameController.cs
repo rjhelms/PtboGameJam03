bf975128ac01da0f83a5abc53f992079d028cece
Rob Hailman <robhailman@gmail.com>
2017-08-05 11:28:21 -0400

    Add two-camera juju

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	[Header("Camera Properties")]
+	public Camera WorldCamera;
+	public Material WorldCameraTexture;
+	public int TargetX;
+	public int TargetY;
+	private float pixelRatioAdjustment;
+
+	// Use this for initialization
+	void Start () {
+		pixelRatioAdjustment = (float)TargetX / (float)TargetY;
+        if (pixelRatioAdjustment <= 1)
+        {
+            WorldCameraTexture.mainTextureScale = 
+				new Vector2(pixelRatioAdjustment, 1);
+            WorldCameraTexture.mainTextureOffset =
+				new Vector2((1 - pixelRatioAdjustment) / 2, 0);
+            WorldCamera.orthographicSize = TargetY / 2;
+        }
+        else
+        {
+            pixelRatioAdjustment = 1f / pixelRatioAdjustment;
+            WorldCameraTexture.mainTextureScale = 
+				new Vector2(1, pixelRatioAdjustment);
+            WorldCameraTexture.mainTextureOffset = 
+				new Vector2(0, (1 - pixelRatioAdjustment) / 2);
+            WorldCamera.orthographicSize = TargetX / 2;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
