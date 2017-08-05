26566f175eec980761ce461a7a7d7c2f66db6f04
Rob Hailman <robhailman@gmail.com>
2017-08-05 14:06:39 -0400

    Add projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public Vector3 MoveVector;
+	public float lifeSpan;
+	private float dieTime;
+	// Use this for initialization
+	void Start () {
+		dieTime = Time.fixedTime + lifeSpan;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		transform.position += MoveVector;
+		if (Time.fixedTime > dieTime)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
