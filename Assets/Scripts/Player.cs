892346e8b8c72c4ead22500a9c6087b7d0a5657a
Rob Hailman <robhailman@gmail.com>
2017-08-05 10:54:10 -0400

    Begin adding controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	int moveSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 moveVector = new Vector2();
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			moveVector += Vector2.right;
+		} else if (Input.GetAxis("Horizontal") < 0)
+		{
+			moveVector += Vector2.left;
+		}
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			moveVector += Vector2.up;
+		} else if (Input.GetAxis("Vertical") < 0)
+		{
+			moveVector += Vector2.down;
+		}
+		Debug.Log(moveVector);
+	}
+}
