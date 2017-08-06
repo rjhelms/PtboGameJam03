bc251ee11c6e91adf7cf2256f39d870a7b51b45c
Rob Hailman <robhailman@gmail.com>
2017-08-06 01:03:51 -0400

    Add Money projectile to Suit

--- Assets/Scripts/Suit.cs
+++ Assets/Scripts/Suit.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class Suit : Enemy
 {
+    public Sprite BaseSprite;
+    public Sprite FireSprite;
+    public GameObject[] Projectile;
+    public Transform FirePoint;
+    public float RestTime = 0.1f;
+    public float FireTime = 0.3f;
+    public float fireChance = 0.25f;
+    private SpriteRenderer spriteRenderer;
+    private bool isFiring;
+    private float nextStateChange;
 
+    protected override void Start()
+    {
+        base.Start();
+        nextStateChange = Time.fixedTime + RestTime;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Time.fixedTime > nextStateChange)
+        {
+            if (!isFiring)
+            {
+                float fireRoll = Random.value;
+                if (fireRoll <= fireChance)
+                {
+                    spriteRenderer.sprite = FireSprite;
+                    nextStateChange += FireTime;
+                    int projectileIndex = Random.Range(0, Projectile.Length);
+                    Instantiate(Projectile[projectileIndex], FirePoint.position,
+                                Quaternion.identity);
+                    isFiring = true;
+                }
+            } else {
+                spriteRenderer.sprite = BaseSprite;
+                nextStateChange += RestTime;
+                isFiring = false;
+            }
+        }
+    }
 }
