2e5bbe39c7f0dda55efa9e6ff82e5920bfac2ff2
Rob Hailman <robhailman@gmail.com>
2017-08-06 12:15:17 -0400

    Finish implementing gameover

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -11,7 +11,7 @@ public class GameOverController : MonoBehaviour {
 	public Text YourScore;
 	public Text HighScore;
 	public Text PlayAgain;
-	private int state = 0;
+	public int state = 0;
 	private float nextTick;
 
 	// Use this for initialization
@@ -22,6 +22,53 @@ public class GameOverController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (state < 5)
+		{
+			if (Time.time >= nextTick)
+			{
+				nextTick += Tick;
+				if (state == 0)
+				{
+					GetAJob.enabled = true;
+					state++;
+				} else if (state == 1)
+				{
+					YourScore.text = "YOUR SCORE\n" + ScoreManager.Instance.Score;
+					YourScore.enabled = true;
+					state++;
+				} else if (state == 2)
+				{
+					int highScore = 0;
+					if(PlayerPrefs.HasKey("highScore"))
+					{
+						highScore = PlayerPrefs.GetInt("highScore");
+					}
+					HighScore.text = "HIGH SCORE\n" + highScore;
+					HighScore.enabled = true;
+					if (ScoreManager.Instance.Score < highScore)
+					{
+						state += 2;
+					} else {
+						highScore = ScoreManager.Instance.Score;
+						PlayerPrefs.SetInt("highScore", highScore);
+						state++;
+					}
+				} else if (state == 3)
+				{
+					HighScore.text = "HIGH SCORE\n" + PlayerPrefs.GetInt("highScore");
+					state++;
+				} else if (state == 4)
+				{
+					PlayAgain.enabled = true;
+					state++;
+				}
+			}
+		} else {
+			if (Input.GetButtonDown("Fire1"))
+			{
+				ScoreManager.Instance.Reset();
+				SceneManager.LoadScene(0);
+			}
+		}
 	}
 }
